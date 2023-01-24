7a7c836fb9e0ef3483882d58636d33a2c5d221d0
Ivan Letov <letov88@gmail.com>
2023-01-24 10:49:56 +0200

    Creat authetication

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -18,5 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myblog.urls'))
+    path('', include('myblog.urls')),
+    path('blogers/', include('django.contrib.auth.urls')),
+    path('blogers/', include('blogers.urls')),
+
 ]
