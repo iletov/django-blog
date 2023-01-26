a465e7e67c4bbae21bd544c0cd5fb0c1a24e835e
Ivan Letov <letov88@gmail.com>
2023-01-27 00:24:19 +0200

    Update the profile page

--- blog/blogers/forms.py
+++ blog/blogers/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 from django import forms
 
@@ -20,3 +20,21 @@ class SignUpForm(UserCreationForm):
 		self.fields['username'].widget.attrs['class'] = 'form-control'
 		self.fields['password1'].widget.attrs['class'] = 'form-control'
 		self.fields['password2'].widget.attrs['class'] = 'form-control'
+
+
+class UpdateProfileForm(UserChangeForm):
+	username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+	first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+	last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+	email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+	# last_login = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+	# date_joined = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+	# is_superuser = forms.CharField(widget=forms.ChechboxInput(attrs={'class': 'form-chech'}))
+	# is_staff = forms.CharField(widget=forms.ChechboxInput(attrs={'class': 'form-chech'}))
+	# is_active = forms.CharField(widget=forms.ChechboxInput(attrs={'class': 'form-chech'}))
+
+	class Meta:
+		model = User
+		fields = ('username', 'first_name', 'last_name', 'email', 'password')
+
+	
--- blog/blogers/views.py
+++ blog/blogers/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
-# from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
-from .forms import SignUpForm
+from .forms import SignUpForm, UpdateProfileForm
 
 
 class UserRegisterView(generic.CreateView):
@@ -10,3 +10,15 @@ class UserRegisterView(generic.CreateView):
 	form_class = SignUpForm
 	template_name = 'registration/register.html'
 	success_url = reverse_lazy('login')
+
+
+# Update the users info
+class UserEditView(generic.UpdateView):
+	form_class = UpdateProfileForm
+	template_name = 'registration/edit_profile.html'
+	success_url = reverse_lazy('home')
+
+	# Get the current users infrmation to update
+	def get_object(self):
+		return self.request.user
+
